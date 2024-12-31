c9ef275117fa137e49bd3dbb85e025463870857f
Ibrahima Sarr <114916109+eutousarr@users.noreply.github.com>
2024-12-31 03:01:32 +0000

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import { getData } from "@/src/actions/todoAction";
+import Todos from "@/src/components/todos";
+
+export default async function Home() {
+  const data = await getData();
+  return <Todos todos={data} />;
+}
