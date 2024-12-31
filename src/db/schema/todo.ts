df9afdb2f6c6eac41fc98b594758f93ff2d32349
Ibrahima Sarr <114916109+eutousarr@users.noreply.github.com>
2024-12-31 03:19:49 +0000

    Update todo.ts

--- src/db/schema/todo.ts
+++ src/db/schema/todo.ts
@@ -1,7 +1,7 @@
 import { integer, text, boolean, pgTable } from "drizzle-orm/pg-core";
 
 export const todo = pgTable("todo", {
-  id: integer("id").primaryKey(),
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(,
   text: text("text").notNull(),
   done: boolean("done").default(false).notNull(),
 });
